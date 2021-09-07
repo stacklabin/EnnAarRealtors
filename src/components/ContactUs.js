b28182d20da2798399b71261409656589935a8d4
Nishant Tyagi <nishant.tyagi.5011@gmail.com>
2021-09-07 16:19:12 +0530

    Update ContactUs.js

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -72,36 +72,7 @@ export default function ContactUs({ calledBy }) {
             <img
               className="background-image lighter"
               src="https://images.unsplash.com/photo-1601752943749-7dd8d89f407a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-              alt="enn arr realtors"
-            />
-            <div className="hidden text-white md:flex flex-col justify-between md:w-2/12 lg:w-4/12">
-              <div className="text-3xl font-bold p-4 pt-8">
-                <p
-                  style={{
-                    writingMode: "vertical-lr",
-                    transform: "rotate(180deg)",
-                  }}
-                >
-                  Contact Us
-                </p>
-                <div className="pt-4 pl-4">
-                  <div
-                    style={{ width: "2px", height: "50px" }}
-                    className="bg-white"
-                  ></div>
-                </div>
-              </div>
-              <div className="flex p-4 justify-center">
-                {/* <a href="" target="_blank" className="m-2">
-                                <FaInstagram/>
-                            </a> */}
-                <a
-                  href="https://www.facebook.com/ennaarrealtors"
-                  target="_blank"
-                  className="m-2"
-                >
-                  <FaFacebook />
-                </a>
+              alt="enn 
                 <a
                   href="https://twitter.com/ENNAARREALTOR"
                   target="_blank"
@@ -120,43 +91,11 @@ export default function ContactUs({ calledBy }) {
             </div>
             <div className="bg-white py-4 w-full md:w-10/12 lg:w-8/12">
               <ModalBody>
-                <div className="text-center">
-                  <h1 className="text-2xl font-bold">Get in Touch !</h1>
-                  <p className="text-sm pt-1">Contact us for assistance</p>
-                  <div className="flex justify-between items-center py-4">
-                    <div className="h-24 w-4/12">
-                      <div className="text-green-600">
-                        <MdLocationOn />
-                      </div>
-                      <div className="text-xs  p-1">
-                        <p>
-                          # 1105, 11th Floor,
-                          <br /> Omaxe India Trade Tower,
-                          <br /> New Chandigarh
-                        </p>
-                      </div>
-                    </div>
-                    <div className="h-24 w-4/12">
-                      <div className="text-green-600">
-                        <MdPhone />
-                      </div>
-                      <div className="text-xs  p-1">
-                        <a href="tel:+919888044333">988-804-4333</a>
-                      </div>
-                    </div>
-                    <div className="h-24 w-4/12">
-                      <div className="text-green-600">
-                        <MdEmail />
-                      </div>
-                      <div className="text-xs  p-1">
-                        <p>ennaarrealtors@gmail.com</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-               <main>
+              
                         <iframe  id="leadTracking" src="https://account.solidperformers.com/captureleads/MTkw" border="0" style="border:0px;height:600px;width:100%"></iframe>
-                        </main>
+                       
+               </div>
+               
 
 {/*<script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.min.js"></script>
