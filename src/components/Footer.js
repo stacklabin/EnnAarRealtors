456fcfe8fad5816856b94fdc2c7891518f8ea2e5
Nishant Tyagi <nishant.tyagi.5011@gmail.com>
2021-09-09 17:20:04 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -307,7 +307,7 @@ export default function Footer() {
                         <path d='M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z'></path>
                       </svg>
                     </span>
-                    &nbsp;# 1105, 11th Floor, Omaxe India Trade Tower, New
+                    &nbsp;# 304-A, 3rd Floor, Omaxe India Trade Tower, New
                     Chandigarh
                   </a>
                 </li>
