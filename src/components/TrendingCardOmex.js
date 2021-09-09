9e7055e58840a9bdb38274b2064909f56a720cfd
Nishant Tyagi <nishant.tyagi.5011@gmail.com>
2021-09-09 13:58:24 +0530

    Update TrendingCardOmex.js

--- src/components/TrendingCardOmex.js
+++ src/components/TrendingCardOmex.js
@@ -78,7 +78,7 @@ export default function TrendingCardOmex({ item }) {
                   <span
                     className="text-xl lg:text-2xl capitalize font-bold pb-1"
                     style={{ width: "90%",
-                             color: 'white'}}
+                             color: "white"}}
                   >
                     {item.name}
                   </span>
@@ -88,7 +88,7 @@ export default function TrendingCardOmex({ item }) {
                       right: "5px",
                       top: "5px",
                       cursor: "pointer",
-                      color: 'white'
+                      color: "white"
                     }}
                   >
                     <RWebShare
@@ -124,7 +124,7 @@ export default function TrendingCardOmex({ item }) {
                 )}
                 <div className="w-full text-lg my-2">
                   <GoLocation />
-                  <span className="pl-2"  style={{ color: 'white'}}>{item.location}</span>
+                  <span className="pl-2"  style={{ color: "white"}}>{item.location}</span>
                 </div>
               </div>
               <div className="flex flex-wrap justify-end  py-2">
@@ -161,7 +161,7 @@ export default function TrendingCardOmex({ item }) {
                   className="bg-green-500 view-button text-white px-3 py-1 ml-2 rounded-xl"
                   // as={}
                   // href="/property/+[title]"
-                  style={{ width: "200px" }}
+                  style={{ width: "200px", background: "black" }}
                   onClick={hadleRoute.bind(this, `/property/${item.name}`)}
                 >
                   View
